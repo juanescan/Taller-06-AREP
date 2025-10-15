2b37b58e5e68a94b366088a022ebfb248282ccf1
Juan <112003348+juanescan@users.noreply.github.com>
2025-10-14 23:01:50 -0500

    Update CorsConfig.java

--- Back/src/main/java/eci/edu/Taller6/config/CorsConfig.java
+++ Back/src/main/java/eci/edu/Taller6/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://taller6homefront.duckdns.org") 
+                        .allowedOrigins("https://taller6homefront.duckdns.org") 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
