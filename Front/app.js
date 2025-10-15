06bd78063dbacb9f5a58c7f6c02938a7bc7bed76
Juan <112003348+juanescan@users.noreply.github.com>
2025-10-14 20:47:39 -0500

    Update app.js

--- Front/app.js
+++ Front/app.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "https://taller6home.duckdns.org";
+const BACKEND_URL = "https://taller6home.duckdns.org:8080";
 const apiUrl = `${BACKEND_URL}/api/properties`;
 let editingId = null;
 
