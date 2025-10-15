9c29513c91cb853b5ff0a4ea34db060a01115577
Juan <112003348+juanescan@users.noreply.github.com>
2025-10-14 21:29:36 -0500

    cambios

--- Back/src/main/java/eci/edu/Taller6/config/CorsConfig.java
+++ Back/src/main/java/eci/edu/Taller6/config/CorsConfig.java
@@ -7,13 +7,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://taller6homefront.duckdns.org") 
+                        .allowedOrigins(
+                            "http://taller6homefront.duckdns.org",
+                            "http://taller6homefront.duckdns.org/"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
