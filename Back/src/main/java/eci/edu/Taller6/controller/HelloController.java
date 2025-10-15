608e332cdca0b796e5fbd7eb76fcb643b2d6ef69
Juan <112003348+juanescan@users.noreply.github.com>
2025-10-14 21:03:19 -0500

    pruebas

---
+++ Back/src/main/java/eci/edu/Taller6/controller/HelloController.java
@@ -0,0 +1,13 @@
+package eci.edu.Taller6.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hola Mundo desde el backend!";
+    }
+}
